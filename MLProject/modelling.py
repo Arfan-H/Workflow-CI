c5947c9c5a029ed51639efdbfb4bad7b4a1ae5fb
hpmini21 <hpmini321@gmail.com>
2025-12-22 20:37:51 +0700

    Add requirements.txt for CI

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -21,7 +21,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-with mlflow.start_run():
+with mlflow.start_run(nested=True):
     model = RandomForestRegressor(
         n_estimators=args.n_estimators,
         random_state=42
